7fefd68e4353d00d0bc163ca30489340b0107a28
mrnonoah0 <noahvellemans@gmail.com>
2025-07-12 23:40:58 +0200

    changes

--- Main.cs
+++
@@ -1,8 +0,0 @@
-int X = 1;
-int Y = 2;
-int Z = 3;
-
-static void Main() {
-    int result = X + Y + Z;
-    Console.WriteLine("The result is: " + result);
-}
---
+++ Main.py
@@ -0,0 +1,11 @@
+numbers = ["n", "i", "g", "e", "r"]
+
+def Main():
+    for number in numbers:
+        if number == "g":
+            print(number)
+            print(number)
+        else:
+            print(number)
+            
+Main()
